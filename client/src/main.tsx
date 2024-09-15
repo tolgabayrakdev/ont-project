b38a20d90f862b57bf0dc25e0ea0f4e0c5960f02
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-15 20:29:41 +0300

    new ui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,9 @@
-import { StrictMode, Suspense } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import routes from './routes'
-import Loading from './components/loading'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Suspense fallback={<Loading />}>
-      <RouterProvider router={routes} />
-    </Suspense>
+    <App />
   </StrictMode>,
 )
--- client/src/pages/error/not-found.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {}
-
-export default function NotFound({ }: Props) {
-    return (
-        <div>404 - Not Found</div>
-    )
-}
