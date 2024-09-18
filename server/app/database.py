329a0120478d97e30d68efbfa80555cb5cf22815
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-18 19:32:31 +0300

    url changed

--- server/app/database.py
+++ server/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://ont-app_owner:cj1U6pZYtJEK@ep-polished-thunder-a2ucc35g.eu-central-1.aws.neon.tech/ont-app?sslmode=require"
+SQLALCHEMY_DATABASE_URL = "postgresql://root:root@localhost/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
