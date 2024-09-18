6f1504036dd733ac4bbef60531ea00d7d0cd00a1
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-18 13:24:29 +0300

    welcome page

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom"
+
 type Props = {}
 
 export default function Home({}: Props) {
   return (
-    <div>Hosgeldin.</div>
+    <div>
+      <h1>Home</h1>
+      <Link to="sign-in">Giriş Yap</Link>
+    </div>
+    
   )
 }
