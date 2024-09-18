59449dce59690077a811a28afffd48c35f221557
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-18 19:23:43 +0300

    loading theme

--- client/src/components/loading.tsx
+++ client/src/components/loading.tsx
@@ -1,9 +1,12 @@
-import { Center, Loader } from '@mantine/core';
+import { Center, Loader, useMantineColorScheme } from '@mantine/core';
 
 export default function Loading() {
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+
   return (
     <Center style={{ width: '100vw', height: '100vh' }}>
-      <Loader size="lg" variant="bars" c="dark" />
+      <Loader size="lg" variant="bars" color={dark ? 'white' : 'dark'} />
     </Center>
   );
 }
